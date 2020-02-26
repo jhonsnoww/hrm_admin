ec029dc5acb4e3cfdd33ea7f2908e49eac104d14
oochan <oochanbg@gmail.com>
2020-02-26 16:41:28 +0630

    Add Vuex

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import employees from './modules/employees'
+Vue.use(Vuex)
+
+new Vuex.Store({
+    modules:{
+        employees
+    }
+})
---
+++ src/store/modules/employees.js
@@ -0,0 +1,14 @@
+const states = {
+}
+const mutations = {}
+
+const actions = {}
+
+const getters = {}
+
+export default {
+    states,
+    mutations,
+    actions,
+    getters
+}
